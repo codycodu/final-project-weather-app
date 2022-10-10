cddca4d46d4207badf1032e176bf669e0bc693f5
codycodu <codydenisa16@gmail.com>
2022-10-10 11:24:28 +0100

    javascript code added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,95 @@
-alert("Hello");
+//temperature
+
+//Date
+let currentDate = new Date();
+
+console.log(currentDate);
+
+function formatDate() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[currentDate.getDay()];
+
+  let hour = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+
+  let dayAndTime = document.querySelector("#date");
+  dayAndTime.innerHTML = `${day} ${hour}:${minutes}`;
+}
+
+formatDate();
+
+//searching location
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#location").value;
+  searchCity(city);
+}
+/*
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+*/
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Lincoln");
+
+/*
+function signUp(event) {
+  console.log(event);
+  let city = document.querySelector("#location").value;
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showTemperature);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", signUp);
+let form = document.querySelector("form");
+form.addEventListener("submit", signUp);
+let searchedLocation = document.querySelector("h1");
+searchedLocation.innerHTML = `${location.value}`;
+*/
